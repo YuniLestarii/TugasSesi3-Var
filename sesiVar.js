13d15ed6a08eb3538725dccda5b378daf4b96e97
YuniLestarii <123483879+YuniLestarii@users.noreply.github.com>
2023-10-20 18:50:24 +0700

    Add files via upload

---
+++ sesiVar.js
@@ -0,0 +1,24 @@
+// Variabel dengan tipe object
+var restaurantObject = {
+    name: "Seoulittle",
+    city: "Sukabumi",
+    favoriteDrink: "Matcha Ice Cube",
+    favoriteFood: "Spicy Chicken",
+    isVegan: false
+};
+
+ /*
+ Variabel dengan tipe array
+ menggunakan kurung kotak sebagai penanda array
+ dan kurung kurawal sebagai pengisi value array
+ */
+var restaurantArray = [
+    {
+        name: "Seoulittle",
+        city: "Sukabumi",
+        favoriteDrink: "Matcha Ice Cube",
+        favoriteFood: "Spicy Chicken",
+        isVegan: false
+    }
+];
+console.log(restaurantObject)
